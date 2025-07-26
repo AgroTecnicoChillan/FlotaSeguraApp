6131c32ad629ddf1c35caa11b6fc8428e1de84df
AgroTecnicoChillan <agrotecnicochillan@gmail.com>
2025-07-26 16:54:08 +0000

    Añadir configuracion de static_files a render.yaml

--- main.py
+++ main.py
@@ -469,6 +469,7 @@ def main(page: ft.Page):
 if __name__ == "__main__":
      ft.app(target=main, assets_dir="assets", view=ft.AppView.WEB_BROWSER)
 
-# Función para iniciar la app    
+# Función para iniciar la app 
+
 
                   
